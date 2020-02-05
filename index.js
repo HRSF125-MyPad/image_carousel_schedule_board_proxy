02add204f2c50eb8f44e72b2bc0ff484180f6df7
garvintan07@gmail.com <garvintan07@gmail.com>
2020-02-04 19:14:06 -0800

    rendering all componenets on proxy

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const axios = require('axios')
+
+app.use(express.json());
+app.use(express.static(__dirname + '/public/'))
+
+app.get('/image', (req, res) => {
+    // console.log(req.url)
+    axios.get(`http://localhost:3002${req.url}`)
+      .then((response) => {
+        // console.log(response.data)
+        res.send(response.data)
+      })
+      .catch((error) => {
+       res.send(error)
+      })
+})
+
+app.get('/homes', (req, res) => {
+    axios.get(`http://localhost:3001/homes`)
+    .then((response) => {
+        console.log(response.data)
+        res.send(response.data)
+    })
+    .catch((error) => {
+        res.send(error)
+    })
+})
+
+app.get('/bank', (req, res) => {
+    axios.get('http://localhost:3001/bank')
+    .then((response) => {
+        res.send(response.data)
+    })
+    .catch((error) => {
+        res.send(error)
+    })
+})
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
